cb653af1c828d02bf947e0095f4d4abb709dfedd
Nico <Warmohawk42@gmail.com>
2025-06-02 15:41:40 +0200

    new

--- Youtube-Videos/Video-7-ttk-Widget.py
+++ Youtube-Videos/Video-7-ttk-Widget.py
@@ -1,8 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
+from PIL import Image, ImageTk
 
 root = tk.Tk()
 root.geometry("400x400")
+
+image = Image.open("C:\Users\Scriptsprache\Desktop\TKinter\Tkinter\Main")
 
 label1 =ttk.Label(root)
 label1.pack()
