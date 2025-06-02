ecfed2b6ddafebaf1df3d61e881fa03d2b3d056f
Nico <Warmohawk42@gmail.com>
2025-06-02 18:01:54 +0200

    yes

--- Youtube-Videos/Video-7-ttk-Widget.py
+++ Youtube-Videos/Video-7-ttk-Widget.py
@@ -3,14 +3,16 @@ from tkinter import ttk
 from PIL import Image, ImageTk
 
 root = tk.Tk()
-root.geometry("400x400")
+root.geometry("800x800")
 
-image = Image.open("Jill.jpg")
+image = Image.open("Youtube-Videos/Jill.jpg") # Unterordner mit angeben falls Bild dort liegt
+image = image.resize((500, 500))
 photo = ImageTk.PhotoImage(image)
 
-label1 =ttk.Label(root, image=photo)
+
+label1 =ttk.Label(root, image=photo, text="Hallo ich bin Jill", compound="top")
 label1.pack()
 
-label1.configure(text="Hello World")
+#label1.configure(text="Hello World")
 
 root.mainloop()
