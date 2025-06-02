da3b195eeb524ed234bc7cbdd41ea1374607c5ec
Nico <Warmohawk42@gmail.com>
2025-06-02 16:08:42 +0200

    new

---
+++ Youtube-Videos/Video-7-Lösung.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+ 
+
+root = tk.Tk()
+root.title("Bildanzeige-Test")
+root.geometry("400x400")  
+ 
+
+bilddatei = "Jill.jpg"
+ 
+try:
+    image = Image.open(bilddatei)
+
+    image = image.resize((300, 300))
+
+    photo = ImageTk.PhotoImage(image)
+
+    label = tk.Label(root, image=photo)
+    label.pack(pady=20)  
+    print("Bild erfolgreich geöffnet und angezeigt!")
+except FileNotFoundError:
+    print(f"Bild '{bilddatei}' nicht gefunden!")
+    label = tk.Label(root, text="Bild nicht gefunden!", bg="red", fg="white")
+    label.pack(pady=20)
+except Exception as e:
+    print("Ein anderer Fehler ist aufgetreten:", e)
+    label = tk.Label(root, text="Fehler beim Laden!", bg="red", fg="white")
+    label.pack(pady=20)
+ 
+
+root.mainloop()
--- Youtube-Videos/Video-7-ttk-Widget.py
+++ Youtube-Videos/Video-7-ttk-Widget.py
@@ -5,9 +5,10 @@ from PIL import Image, ImageTk
 root = tk.Tk()
 root.geometry("400x400")
 
-image = Image.open("C:\Users\Scriptsprache\Desktop\TKinter\Tkinter\Main")
+image = Image.open("Jill.jpg")
+photo = ImageTk.PhotoImage(image)
 
-label1 =ttk.Label(root)
+label1 =ttk.Label(root, image=photo)
 label1.pack()
 
 label1.configure(text="Hello World")
